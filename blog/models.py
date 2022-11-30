efe3cb117c5e2a881a1bcdc49b3c26ec39002609
[Github <[Github>
2022-11-30 15:52:56 +0900

    포스트 목록 페이지와 포스트 상세 페이지에 카테고리 기능추가

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,18 @@ from django.db import models
 from django.contrib.auth.models import User
 import os
 
+
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'categories'
+
+
 class Post(models.Model):
     title = models.CharField(max_length=30)
     hook_text = models.CharField(max_length=100, blank=True)
@@ -15,11 +27,16 @@ class Post(models.Model):
 
     author = models.ForeignKey(User, null=True, on_delete=models.SET_NULL)
 
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
+
+    def get_file_name(self):
+        return os.path.basename(self.file_upload.name)
 
     def get_file_ext(self):
         return self.get_file_name().split('.')[-1]
