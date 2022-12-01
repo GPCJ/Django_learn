8c6dce5f5961a2acf9accd4ba7ddec604d7a55c2
[Github <[Github>
2022-12-01 11:51:47 +0900

    카테고리 페이지 만들기

--- blog/urls.py
+++ blog/urls.py
@@ -4,8 +4,7 @@ from . import views
 
 # FBV방식
 urlpatterns = [
+    path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
-    # blog뒤에 int값이 있있으면 views파일 안에 single_post_page함수 실행
-    path('<int:pk>/', views.single_post_page),
 ]
