2fcb9241e77f146e8c8ebce436d35bb551e3237b
GPCJ <95620390+GPCJ@users.noreply.github.com>
2022-12-05 10:33:47 +0900

    태그 페이지 추가

--- blog/urls.py
+++ blog/urls.py
@@ -4,6 +4,7 @@ from . import views
 
 # FBV방식
 urlpatterns = [
+    path('tag/<str:slug>/', views.tag_page),
     path('category/<str:slug>/', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
